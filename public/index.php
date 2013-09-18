5204d0467b8f6fe69f48eb6ffb07e7f8b7ba36c5
Drew Withers <drew@thewithers.org>
2013-09-18 08:29:24 -0600

    Moved map.html to index.php Consolidated javascript into map.js Added config-example.php placeholder

--- public/index.php
+++ public/index.php
@@ -1,3 +1,27 @@
-<?php
-  phpinfo();
-?>
+﻿<!DOCTYPE html>
+<html>
+    <head>
+        <title></title>
+        <!-- Google Maps API reference -->
+        <script
+            src="https://maps.googleapis.com/maps/api/js?sensor=false&libraries=places,geometry">
+        </script>
+        
+        <!-- map references -->
+        <link href="./map.css" rel="stylesheet" />
+        
+        <script src="./map.js"></script>
+
+    </head>
+    <body>
+        <div id="rootGrid">
+            
+            <div id="mapdisplay"></div>
+            <div id="inputFrame">
+                <input id="start" type="text" size="30" placeholder="Generations..." onkeypress="if (event.keyCode ==13) ancestorgens()"/>
+                
+            </div>
+           
+        </div>
+    </body>
+</html>
--- public/map.js
+++ public/map.js
@@ -3,16 +3,6 @@ var polyarray = [];
 var markarray = [];
 var infoarray = [];
 
-(function () {
-    "use strict";
-
-    function receiveMessage(e) {
-        if (e.origin === "ms-appx://" + window.location.host) {
-            var response = e.data;
-            getLocationPoints(response);
-            
-        }
-    };
 
     function getLocationPoints(locations) {
 
@@ -216,21 +206,162 @@ var infoarray = [];
         //selectMode[0].checked = true;
     }
 
-    
-
 google.maps.event.addDomListener(window, 'load', initialize);
 
-window.addEventListener("message", receiveMessage, false);
+    function getSessionId(gen) {
 
-})();
+        var key = "AAAA-AAAA-AAAA-AAAA-AAAA-AAAA-AAAA-AAAA";
+        var url = "https://sandbox.familysearch.org/identity/v2/login?key=" + key;
+
+		var xhttp;
+		xhttp=new XMLHttpRequest();
+		xhttp.open("GET",url);
+		xhttp.send();
+		var xmlDocument = xhttp.responseXML;
+                var session = xmlDocument.getElementsByTagName("session");
+                var sessionID = session[0].getAttribute("id");
+                ancestors(sessionID,gen);
+
+    }
+
+    function ancestors(id,gen) {
+        var generations = gen;
+        var personId = "KW7F-W25";
+
+        var url = "https://sandbox.familysearch.org/familytree/v2/pedigree/" + personId + "?ancestors=" + generations + "&properties=all&sessionId=" + id;
+
+		var xhttp;
+		xhttp=new XMLHttpRequest();
+		xhttp.open("GET",url);
+		xhttp.send();
+		var xmlDocument = xhttp.responseXML;
+                var persons = xmlDocument.getElementsByTagName("person");
+                var tags = new Array();
+                
+                tags[0] = persons[0].getAttribute("id");
+                for (var i = 0; i < Math.pow(2, generations) - 1; i++) {
+                    
+                    var parents = persons[i].getElementsByTagName("parent");
+                    if (parents.length > 1) {
+                        tags[(i + 1) * 2 - 1] = parents[0].getAttribute("id");
+                        tags[(i + 1) * 2] = parents[1].getAttribute("id");
+                    } else {
+                        tags[(i + 1) * 2 - 1] = "";
+                        tags[(i + 1) * 2] = "";
+                    }
+                    //if (i < Math.pow(2, generations - 1) - 1) {
+                    //    if (persons[(i + 1) * 2 - 1].getAttribute("id") !== parents[1].getAttribute("id")) {
+                    //        persons[(i + 1) * 2 - 1]
+                    //    }
+                    //}
+                }
+
+                getLocations(tags, id);
+    }
+
+    function getLocations(persons,sessionId) {
+        var people = persons.length;
+        var locations = new Array();
+
+        asyncLoop(people, function (loop) {
+
+            var idx = loop.iteration();
+            var id = persons[idx];
+            
+            locationPromise(id,sessionId, function (result) {
+
+                // log the iteration
+                locations[loop.iteration()] = result;
+                if (result == "") {
+                    if ((loop.iteration() + 1) / 2 == Math.round((loop.iteration() + 1) / 2)) { // male
+
+                        locations[loop.iteration()] = locations[(loop.iteration() + 1) / 2 - 1];
+                    } else { // female ancestor
+
+                        locations[loop.iteration()] = locations[(loop.iteration()) / 2 - 1];
+                    }
+                }
+                // Okay, for cycle could continue
+                loop.next();
+            })
+            },
+        function () {
+	    getLocationPoints(locations);
+                    }
+        );
+    }
+
+    function asyncLoop(iterations, func, callback) {
+        var index = 0;
+        var done = false;
+        var loop = {
+            next: function () {
+                if (done) {
+                    return;
+                }
+
+                if (index < iterations) {
+                    index++;
+                    func(loop);
+
+                } else {
+                    done = true;
+                    callback();
+                }
+            },
+
+            iteration: function () {
+                return index - 1;
+            },
+
+            break: function () {
+                done = true;
+                callback();
+            }
+        };
+        loop.next();
+        return loop;
+    }
+
+    function locationPromise(id, sessionId, callback) {
+
+        var url = "https://sandbox.familysearch.org/familytree/v2/person/" + id + "?&events=standard&sessionId=" + sessionId;
+
+        if (id !== "") {
+		var xhttp;
+		xhttp=new XMLHttpRequest();
+		xhttp.open("GET",url);
+		xhttp.send();
+		var xmlDocument = xhttp.responseXML;
+            
+                var locString = "";
+                var events = xmlDocument.getElementsByTagName("events");
+                if (events[0]) {
+
+                	var places = events[0].getElementsByTagName("place");
+
+			if (places[0]) {
+				var locString = places[0].childNodes[0].innerText;
+                        }
+                 }
+
+                 callback(locString);
+
+        } else {
+            callback(id);
+        }
+
+    }
 
 
-function ancestors() {
+
+
+function ancestorgens() {
     clearOverlays();
 
     var start = document.getElementById('start');
     var gen = parseFloat(start.value);
-    window.parent.postMessage(gen, "*");
+    getSessionId(gen);
 
 }
 
