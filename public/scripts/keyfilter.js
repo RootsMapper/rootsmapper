a34f9027431f3f825681900907eee75525d23a85
Drew Withers <drew@thewithers.org>
2013-09-29 13:34:17 -0600

    Removed compiled files

--- public/scripts/keyfilter.js
+++
@@ -1,1 +0,0 @@
-function alphaFilterKeypress(a){a=a||window.event;a=String.fromCharCode(a.keyCode||a.which);return/[a-z0-9\-]/i.test(a)}window.onload=function(){document.getElementById("personid").onkeypress=alphaFilterKeypress};
