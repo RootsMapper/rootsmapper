3e95ec6efe7e87b0c33133ac4d58e43a91ba5b96
Drew Withers <drew@thewithers.org>
2013-10-17 11:00:44 -0600

    Refresh page to current folder rather than index.php

--- public/index.php
+++ public/index.php
@@ -34,7 +34,7 @@ if( isset($_REQUEST['code']) ) {
 	  session_regenerate_id(true); //Regenerate session ID
 	  $_SESSION['fs-session'] = $fs->GetAccessToken($DEV_KEY, $_REQUEST['code']); //Store access code in session variable
 	  $_SESSION['fingerprint'] = md5($fingerprint);
-	  header('Location: ' . basename(__FILE__)); //Refresh page to clear POST variables
+	  header('Location: ' . $_SERVER['SERVER_NAME'] . dirname($_SERVER['PHP_SELF'])); //Refresh page to clear POST variables
 	  exit;
 } 
 
