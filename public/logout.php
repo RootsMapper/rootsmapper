9cfd6a34a9f79d932f63ae4bb16c24c286155e05
Drew Withers <drew@thewithers.org>
2013-10-17 11:01:33 -0600

    Changed redirect from index.php to /

--- public/logout.php
+++ public/logout.php
@@ -4,6 +4,6 @@ session_start();
 unset($_SESSION['fs-session']); //clear session variable
 unset($_SESSION['fingerprint']); //clear fingerprint variable
 session_destroy();
-header('Location: ' . 'index.php'); //redirect back to index.php
+header('Location: ' . $_SERVER['SERVER_NAME'] . dirname($_SERVER['PHP_SELF'])); //redirect back to main page
 
 ?>
