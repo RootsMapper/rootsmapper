e9a3bc830159c3da950b63e656ba163820977978
Drew Withers <drew@thewithers.org>
2013-10-17 12:36:51 -0600

    Missed a spot

--- public/logout.php
+++ public/logout.php
@@ -4,6 +4,6 @@ session_start();
 unset($_SESSION['fs-session']); //clear session variable
 unset($_SESSION['fingerprint']); //clear fingerprint variable
 session_destroy();
-header('Location: /'); //redirect back to main page
+header('Location: ./'); //redirect back to main page
 
 ?>
