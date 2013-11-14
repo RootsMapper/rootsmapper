055d9af206fee6e8f302d810a4a105aed744d0db
Drew Withers <drew@thewithers.org>
2013-11-14 14:31:56 -0700

    jquery 1.6.4 to 1.4.2

--- public/index.php
+++ public/index.php
@@ -71,7 +71,7 @@ else
 	<!-- map references -->
 
 	<!-- loading animation references -->
-	<script type="text/javascript" src="//ajax.googleapis.com/ajax/libs/jquery/1.6.4/jquery.min.js"></script>
+	<script type="text/javascript" src="//ajax.googleapis.com/ajax/libs/jquery/1.4.2/jquery.min.js"></script>
 	<script type="text/javascript" src="scripts/loading.js?v=<?php echo isset($VERSION)? $VERSION : ""; ?>"></script>
 	<!-- loading animation references -->
 		<script src="scripts/binarytree.js?v=<?php echo isset($VERSION)? $VERSION : ""; ?>"></script>
